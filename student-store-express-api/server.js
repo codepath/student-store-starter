973107caac591666bfdd37f7e331ffe2c064b79e
vincechen2 <Vincent.chen.1@stonybrook.edu>
2022-05-27 15:13:23 -0500

    Deploying express api

--- student-store-express-api/server.js
+++ student-store-express-api/server.js
@@ -1,33 +1,33 @@
-const express = require("express")
-const morgan = require("morgan")
-const cors = require("cors")
-const { NotFoundError } = require("./utils/errors")
-const storeRouter = require("./routes/store")
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { NotFoundError } = require("./utils/errors");
+const storeRouter = require("./routes/store");
 
-const app = express()
+const app = express();
 
-app.use(morgan("tiny"))
-app.use(express.json())
-app.use(cors())
-app.use("/store", storeRouter)
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors());
+app.use("/store", storeRouter);
 
 /* Handle all 404 errors that weren't matched by a route */
 app.use((req, res, next) => {
-  return next(new NotFoundError())
-})
+  return next(new NotFoundError());
+});
 
 /* Generic error handler - anything that is unhandled will be handled here */
 app.use((error, req, res, next) => {
-  const status = error.status || 500
-  const message = error.message
+  const status = error.status || 500;
+  const message = error.message;
 
   return res.status(status).json({
     error: { message, status },
-  })
-})
+  });
+});
 
-const port = 3001
+const port = 3001;
 
 app.listen(port, () => {
-  console.log(`🚀 Server listening on port ` + port)
-})
+  console.log(`🚀 Server listening on port ` + port);
+});
